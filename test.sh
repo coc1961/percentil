6a004251487edcdedfb1c8a98906a7a5fa7d6b74
Carlos Castro <carlos_o_castro@yahoo.com.ar>
2019-04-14 21:16:07 -0300

    Modifico calculo

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+go get -u github.com/coc1961/go/trs
+
 data=$(/bin/echo "3,6,7,8,8,10,13,15,16,20" | trs "," "\\n")
 for a in $(seq 99); do res=`echo $data | go run percentil.go -f - -p $a`; echo "perc p$a=$res"; done
