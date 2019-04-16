4eaf36958f90e6bd04227bccc2baef130d9ae8e8
Carlos Castro <carlos_o_castro@yahoo.com.ar>
2019-04-15 21:16:06 -0300

    Modifico calculo

--- test.sh
+++ test.sh
@@ -2,5 +2,29 @@
 
 go get -u github.com/coc1961/go/trs
 
-data=$(/bin/echo "3,6,7,8,8,10,13,15,16,20" | trs "," "\\n")
-for a in $(seq 99); do res=`echo $data | go run percentil.go -f - -p $a`; echo "perc p$a=$res"; done
+printPersentil(){
+    data=$(/bin/echo $1 | trs ";" "\\n" )
+    for a in $(seq 99)
+    do 
+        res=`echo $data | go run percentil.go -f - -p $a`; echo "perc p$a=$res"
+    done
+}
+
+
+#printPersentil "3;6;7;8;8;10;13;15;16;20"
+
+if [  -n "$1"  ]; 
+then 
+    printPersentil $1
+else 
+    echo "Ejecutar: ---> $0 \"numeros....\""
+    echo "          |"
+    echo "          ---> Ejemplo $0 \"3;4;5;6;7;8;9\""
+fi
+
+
+# Ejemplo con datos agrupados
+# ./test.sh "200,85;300,90;400,120;500,70;600,62;700,36"
+
+# Ejemplo con datos no agrupados
+# ./test.sh "3;6;7;8;8;10;13;15;16;20"
